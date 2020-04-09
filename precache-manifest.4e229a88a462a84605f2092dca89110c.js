ac5e8315fecae3e5fd2a46b0120843f335042c32
shai moalem <shaimuli@gmail.com>
2020-04-09 23:58:41 +0300

    Updates

---
+++ precache-manifest.4e229a88a462a84605f2092dca89110c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1768d4481cccc008a81dd39502353f",
+    "url": "/shaimuli/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "96a6fc800156960fa62f",
+    "url": "/shaimuli/monsters_rolodex/static/css/main.53961641.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/shaimuli/monsters_rolodex/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "96a6fc800156960fa62f",
+    "url": "/shaimuli/monsters_rolodex/static/js/main.d1bb2caf.chunk.js"
+  },
+  {
+    "revision": "967014f167e9e1d83775",
+    "url": "/shaimuli/monsters_rolodex/static/js/runtime~main.8050c903.js"
+  }
+]);
